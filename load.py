0b135100f27ed9c495d3f125d3c28fafd555f452
Malte Harland <mharland3@gmail.com>
2018-09-11 11:31:21 +0200

    load checks also for class_name

--- load.py
+++ load.py
@@ -2,8 +2,8 @@ import h5py, numpy as np
 
 
 class LoadedResult:
-    def __init__(self, fname, groupname = 'scstiffness', class_name = 'SCStiffness3D', nk = 32 , niw = 50, tnn = -1,
-                 tnnn = 0.3, tz = -.15):
+    def __init__(self, fname, groupname = 'scstiffness', class_name = 'SCStiffness3D', nk = 32,
+                 niw = 50, tnn = -1, tnnn = 0.3, tz = -.15):
         self.params = {'nk': nk, 'niw': niw, 'tnn': tnn, 'tnnn': tnnn, 'tz': tz}
         self.values = {}
         self.result_loaded = False
@@ -16,15 +16,20 @@ class LoadedResult:
                 for run_, run in h5f[gname].items():
                     if run_ == 'n': continue
                     params_found = True
-                    for parname, par in run['parameters'].items():
-                        if self.params[parname] != par.value:
-                            params_found = False
-                            break
+                    
+                    if run['class_name'].value != class_name:
+                        params_found = False
+                    else:
+                        for parname, par in run['parameters'].items():
+                            if self.params[parname] != par.value:
+                                params_found = False
+                                break
                     if params_found:
                         self.class_name = run['class_name'].value
                         for res_, res in run['result_keys'].items():
                             self.values.update({run['result_keys'][res_].value: run['result_values'][res_].value})
                         self.result_loaded = True
+                        break
 
     def __getitem__(self, key):
         val = None
