c65b2c7203431cf87bd1d6b449682bb1e57204ef
Harland, Malte <malte.harland@lhind.dlh.de>
2020-08-16 13:53:48 +0200

    port to triqs 2.2.x; refactor

--- scstiffness/I.py
+++ scstiffness/I.py
@@ -1,7 +1,8 @@
-import numpy as np, itertools as itt
+import numpy as np
+import itertools as itt
 
 from pytriqs.archive import HDFArchive
-from pytriqs.gf.local import BlockGf, GfImFreq
+from pytriqs.gf import BlockGf, GfImFreq
 from pytriqs.utility import mpi
 
 from j import JosephsonExchangeCommon
@@ -13,10 +14,12 @@ class SCStiffnessCommon(JosephsonExchangeCommon):
     """
     The theory is gauge invariant, thus J_loc = 0
     """
-    def __init__(self, h5name_cdmft, nk, niw, tnn, tnnn, tz = -.15, verbose = True, xx = True, xy = False, xz = False, zz = True, loops = [-1], gk_on_the_fly = True, hk_on_the_fly = True, j_loc = False):
+
+    def __init__(self, h5name_cdmft, nk, niw, tnn, tnnn, tz=-.15, verbose=True, xx=True, xy=False, xz=False, zz=True, loops=[-1], gk_on_the_fly=True, hk_on_the_fly=True, j_loc=False):
 
         self.h5name_cdmft = h5name_cdmft
-        self.parameters = {'nk':nk, 'niw': niw, 'tnn': tnn, 'tnnn': tnnn, 'tz': tz}
+        self.parameters = {'nk': nk, 'niw': niw,
+                           'tnn': tnn, 'tnnn': tnnn, 'tz': tz}
         self.verbose = verbose
         self.values = {}
 
@@ -30,34 +33,34 @@ class SCStiffnessCommon(JosephsonExchangeCommon):
     def calc_values(self, se, glat, xx, xy, xz, zz, j_loc):
         niw = self.parameters['niw']
         tnn, tnnn, tz = self.parameters['tnn'], self.parameters['tnnn'], self.parameters['tz']
-        beta = se.beta
+        beta = se.mesh.beta
 
         # init accumulators
-        jloc = GfImFreq(beta = beta, n_points = niw, indices = [0])
-        jloc00 = jloc[0,0]
-        rhoxx = GfImFreq(beta = beta, n_points = niw, indices = [0])
-        rhoxx00 = rhoxx[0,0]
-        rhoxy = GfImFreq(beta = beta, n_points = niw, indices = [0])
-        rhoxy00 = rhoxy[0,0]
-        rhoxz = GfImFreq(beta = beta, n_points = niw, indices = [0])
-        rhoxz00 = rhoxz[0,0]
-        rhozz = GfImFreq(beta = beta, n_points = niw, indices = [0])
-        rhozz00 = rhozz[0,0]
-        dgdkx = GfImFreq(beta = beta, n_points = niw, indices = range(8))
-        dgdky = GfImFreq(beta = beta, n_points = niw, indices = range(8))
-        dgdkz = GfImFreq(beta = beta, n_points = niw, indices = range(8))
-        
+        jloc = GfImFreq(beta=beta, n_points=niw, indices=[0])
+        jloc00 = jloc[0, 0]
+        rhoxx = GfImFreq(beta=beta, n_points=niw, indices=[0])
+        rhoxx00 = rhoxx[0, 0]
+        rhoxy = GfImFreq(beta=beta, n_points=niw, indices=[0])
+        rhoxy00 = rhoxy[0, 0]
+        rhoxz = GfImFreq(beta=beta, n_points=niw, indices=[0])
+        rhoxz00 = rhoxz[0, 0]
+        rhozz = GfImFreq(beta=beta, n_points=niw, indices=[0])
+        rhozz00 = rhozz[0, 0]
+        dgdkx = GfImFreq(beta=beta, n_points=niw, indices=range(8))
+        dgdky = GfImFreq(beta=beta, n_points=niw, indices=range(8))
+        dgdkz = GfImFreq(beta=beta, n_points=niw, indices=range(8))
+
         glatk = scatter_list(glat.k)
         nk_core = len(glatk)
         glatwk = scatter_list(glat.wk)
         glatik = scatter_list(glat.ik)
         twopi = np.pi*2
-        p3 = np.array([[1,0],[0,-1]])
+        p3 = np.array([[1, 0], [0, -1]])
         se0 = se['0']
 
         for i_k, kv, wk in zip(glatik, glatk, glatwk):
             self.report_detail('i_k = '+str(i_k+1)+'/'+str(nk_core))
-            depsargs = [kv*twopi,tnn,tnnn,tz]
+            depsargs = [kv*twopi, tnn, tnnn, tz]
             if xx or xy or xz:
                 depsdkx = np.kron(p3, self.hop.deps_by_dkx(*depsargs))
                 dgdkx.zero()
@@ -67,45 +70,52 @@ class SCStiffnessCommon(JosephsonExchangeCommon):
             if xz or zz:
                 depsdkz = np.kron(p3, self.hop.deps_by_dkz(*depsargs))
                 dgdkz.zero()
-            
+
             glatgki = glat.gk[i_k]['0']
 
             for i, j, m, n in itt.product(*[range(8)]*4):
-                if i >= 4 and n < 4: continue # anomalous symmetry
-                if j >= 4 and m < 4: continue # no anomalous dispersion
-                if m >= 4 and j < 4: continue #
+                if i >= 4 and n < 4:
+                    continue  # anomalous symmetry
+                if j >= 4 and m < 4:
+                    continue  # no anomalous dispersion
+                if m >= 4 and j < 4:
+                    continue
                 # only these entries are needed below (also numerically checked)
-                if not((i,n) in [(1,5),(1,1),(5,5),(1,6),(2,5),(1,2),(6,5),(2,1),(5,6),(2,6),
-                                 (2,2),(6,6)]): continue 
+                if not((i, n) in [(1, 5), (1, 1), (5, 5), (1, 6), (2, 5), (1, 2), (6, 5), (2, 1), (5, 6), (2, 6),
+                                  (2, 2), (6, 6)]):
+                    continue
                 if xx or xy or xz:
-                    dgdkx[i,n] += glatgki[i,j] * depsdkx[j,m] * glatgki[m,n]
+                    dgdkx[i, n] += glatgki[i, j] * \
+                        depsdkx[j, m] * glatgki[m, n]
                 if xy:
-                    dgdky[i,n] += glatgki[i,j] * depsdky[j,m] * glatgki[m,n]
+                    dgdky[i, n] += glatgki[i, j] * \
+                        depsdky[j, m] * glatgki[m, n]
                 if xz or zz:
-                    dgdkz[i,n] += glatgki[i,j] * depsdkz[j,m] * glatgki[m,n]
+                    dgdkz[i, n] += glatgki[i, j] * \
+                        depsdkz[j, m] * glatgki[m, n]
 
             # S has only two entries: XX, YY
-            for i, j, k, l in [(1,1,1,1),(1,2,2,1),(2,1,1,2),(2,2,2,2)]:
+            for i, j, k, l in [(1, 1, 1, 1), (1, 2, 2, 1), (2, 1, 1, 2), (2, 2, 2, 2)]:
                 if xx:
-                    rhoxx00 += wk*(dgdkx[0+i,4+j]*se0[0+j,4+k]*dgdkx[0+k,4+l]*se0[0+l,4+i]-
-                                   dgdkx[0+i,0+j]*se0[0+j,4+k]*dgdkx[4+k,4+l]*se0[0+l,4+i])
+                    rhoxx00 += wk*(dgdkx[0+i, 4+j]*se0[0+j, 4+k]*dgdkx[0+k, 4+l]*se0[0+l, 4+i] -
+                                   dgdkx[0+i, 0+j]*se0[0+j, 4+k]*dgdkx[4+k, 4+l]*se0[0+l, 4+i])
                 if xy:
-                    rhoxy00 += wk*(dgdkx[0+i,4+j]*se0[0+j,4+k]*dgdky[0+k,4+l]*se0[0+l,4+i]-
-                                   dgdkx[0+i,0+j]*se0[0+j,4+k]*dgdky[4+k,4+l]*se0[0+l,4+i]-
-                                   dgdky[0+i,0+j]*se0[0+j,4+k]*dgdkx[4+k,4+l]*se0[0+l,4+i])
+                    rhoxy00 += wk*(dgdkx[0+i, 4+j]*se0[0+j, 4+k]*dgdky[0+k, 4+l]*se0[0+l, 4+i] -
+                                   dgdkx[0+i, 0+j]*se0[0+j, 4+k]*dgdky[4+k, 4+l]*se0[0+l, 4+i] -
+                                   dgdky[0+i, 0+j]*se0[0+j, 4+k]*dgdkx[4+k, 4+l]*se0[0+l, 4+i])
                 if xz:
-                    rhoxz00 += wk*(dgdkx[0+i,4+j]*se0[0+j,4+k]*dgdkz[0+k,4+l]*se0[0+l,4+i]-
-                                   dgdkx[0+i,0+j]*se0[0+j,4+k]*dgdkz[4+k,4+l]*se0[0+l,4+i]-
-                                   dgdkz[0+i,0+j]*se0[0+j,4+k]*dgdkx[4+k,4+l]*se0[0+l,4+i])
+                    rhoxz00 += wk*(dgdkx[0+i, 4+j]*se0[0+j, 4+k]*dgdkz[0+k, 4+l]*se0[0+l, 4+i] -
+                                   dgdkx[0+i, 0+j]*se0[0+j, 4+k]*dgdkz[4+k, 4+l]*se0[0+l, 4+i] -
+                                   dgdkz[0+i, 0+j]*se0[0+j, 4+k]*dgdkx[4+k, 4+l]*se0[0+l, 4+i])
                 if zz:
-                    rhozz00 += wk*(dgdkz[0+i,4+j]*se0[0+j,4+k]*dgdkz[0+k,4+l]*se0[0+l,4+i]-
-                                   dgdkz[0+i,0+j]*se0[0+j,4+k]*dgdkz[4+k,4+l]*se0[0+l,4+i])
+                    rhozz00 += wk*(dgdkz[0+i, 4+j]*se0[0+j, 4+k]*dgdkz[0+k, 4+l]*se0[0+l, 4+i] -
+                                   dgdkz[0+i, 0+j]*se0[0+j, 4+k]*dgdkz[4+k, 4+l]*se0[0+l, 4+i])
                 if j_loc:
-                    jloc00 += wk*(glatgki[0+i,0+j]*se0[0+j,4+k]*glatgki[4+k,4+l]*se0[0+l,4+i]-
-                                  glatgki[0+i,4+j]*se0[0+j,4+k]*glatgki[0+k,4+l]*se0[0+l,4+i])
-            for i, j in [(1,1),(1,2),(2,1),(2,2)]:
+                    jloc00 += wk*(glatgki[0+i, 0+j]*se0[0+j, 4+k]*glatgki[4+k, 4+l]*se0[0+l, 4+i] -
+                                  glatgki[0+i, 4+j]*se0[0+j, 4+k]*glatgki[0+k, 4+l]*se0[0+l, 4+i])
+            for i, j in [(1, 1), (1, 2), (2, 1), (2, 2)]:
                 if j_loc:
-                    jloc00 += (-1)*wk*(glatgki[0+i,4+j]*se0[0+j,4+i])
+                    jloc00 += (-1)*wk*(glatgki[0+i, 4+j]*se0[0+j, 4+i])
         if xx:
             rhoxx << mpi.all_reduce(mpi.world, rhoxx, lambda x, y: x + y)
             self.values['xx'] = rhoxx.total_density().real
@@ -122,7 +132,7 @@ class SCStiffnessCommon(JosephsonExchangeCommon):
             jloc << mpi.all_reduce(mpi.world, jloc, lambda x, y: x + y)
             self.values['j_loc'] = jloc.total_density().real
 
-    def save_to_h5(self, h5name = None, groupname = 'scstiffness'):
+    def save_to_h5(self, h5name=None, groupname='scstiffness'):
         if h5name is None:
             self._save(self.h5name_cdmft, groupname)
         else:
@@ -131,11 +141,12 @@ class SCStiffnessCommon(JosephsonExchangeCommon):
 
 class SCStiffness2D(SCStiffnessCommon):
 
-    def __init__(self, h5name_cdmft, nk, niw, tnn, tnnn, tz = 0, verbose = True, xx = True, xy = False, loops = [-1], gk_on_the_fly = True, hk_on_the_fly = True, j_loc = False):
+    def __init__(self, h5name_cdmft, nk, niw, tnn, tnnn, tz=0, verbose=True, xx=True, xy=False, loops=[-1], gk_on_the_fly=True, hk_on_the_fly=True, j_loc=False):
 
         tz = 0
         self.h5name_cdmft = h5name_cdmft
-        self.parameters = {'nk':nk, 'niw': niw, 'tnn': tnn, 'tnnn': tnnn, 'tz': tz}
+        self.parameters = {'nk': nk, 'niw': niw,
+                           'tnn': tnn, 'tnnn': tnnn, 'tz': tz}
         self.verbose = verbose
         self.values = {}
 
--- scstiffness/j.py
+++ scstiffness/j.py
@@ -1,7 +1,8 @@
-import numpy as np, itertools as itt
+import numpy as np
+import itertools as itt
 
 from pytriqs.archive import HDFArchive
-from pytriqs.gf.local import BlockGf, GfImFreq, GfImTime, inverse
+from pytriqs.gf import BlockGf, GfImFreq, GfImTime, inverse
 from pytriqs.utility import mpi
 
 from cdmft.h5interface import Storage
@@ -13,28 +14,31 @@ from periodization.generic import LocalLatticeGreensfunction
 from symmetry import symmetrize
 from hoppings import Hopping2D, Hopping3D, Hopping3DAndersen
 
-    
+
 class JosephsonExchangeCommon:
     """
     Mu has to be in electron-dispersion due to nambu-basis
     ..._on_the_fly(=True) is slower, but less memory consuming
     """
-    def __init__(self, h5name_cdmft, nk, niw, tnn, tnnn, tz = -.15,
-                 rjs = [(0,0,0),(1,0,0),(0,0,1),(1,1,0),(1,0,1),(1,1,1),(2,0,0),(0,0,2),(3,0,0),
-                        (0,0,3)],
-                 verbose = True, loops = [-1], gk_on_the_fly = True, hk_on_the_fly = True):
+
+    def __init__(self, h5name_cdmft, nk, niw, tnn, tnnn, tz=-.15,
+                 rjs=[(0, 0, 0), (1, 0, 0), (0, 0, 1), (1, 1, 0), (1, 0, 1), (1, 1, 1), (2, 0, 0), (0, 0, 2), (3, 0, 0),
+                      (0, 0, 3)],
+                 verbose=True, loops=[-1], gk_on_the_fly=True, hk_on_the_fly=True):
 
         self.h5name_cdmft = h5name_cdmft
-        self.parameters = {'nk':nk, 'niw': niw, 'tnn': tnn, 'tnnn': tnnn, 'tz': tz}
+        self.parameters = {'nk': nk, 'niw': niw,
+                           'tnn': tnn, 'tnnn': tnnn, 'tz': tz}
         self.verbose = verbose
         self.values = dict()
         self.fsfs = dict()
         self.gsgs = dict()
-        
+
         se_cdmft, mu = self.load_cdmft(h5name_cdmft, niw, loops)
         se_cdmft = self.transf_sp_basis(se_cdmft)
         hop = self.get_hopping(mu, tnn, tnnn, tz)
-        se, glat = self.calc_correlation_functions(se_cdmft, hop, nk, gk_on_the_fly, hk_on_the_fly)
+        se, glat = self.calc_correlation_functions(
+            se_cdmft, hop, nk, gk_on_the_fly, hk_on_the_fly)
         self.calc_values(se, glat, rjs)
 
     def report(self, text):
@@ -52,14 +56,16 @@ class JosephsonExchangeCommon:
         mu = sto.load('mu')
         return se, mu
 
-    def load_g(self, name, sto, niw, loops, symmetrize_g = True):
+    def load_g(self, name, sto, niw, loops, symmetrize_g=True):
         gimpin = sto.load(name, -1)
-        gimp = BlockGf(name_block_generator = [(s, GfImFreq(beta = gimpin.beta, n_points = niw, indices = b.indices)) for s, b in gimpin])
+        gimp = BlockGf(name_block_generator=[(s, GfImFreq(
+            beta=gimpin.mesh.beta, n_points=niw, indices=b.indices)) for s, b in gimpin], make_copies=False)
         gimptmp = gimp.copy()
         for loop in loops:
             gimpin = sto.load(name, loop)
             for bn, b in gimpin:
-                gtau = GfImTime(indices = b.indices, n_points = 10001, beta = b.beta)
+                gtau = GfImTime(indices=b.indices,
+                                n_points=10001, beta=b.mesh.beta)
                 gtau.set_from_inverse_fourier(gimpin[bn])
                 gimptmp[bn].set_from_fourier(gtau)
             gimp += gimptmp/len(loops)
@@ -68,10 +74,10 @@ class JosephsonExchangeCommon:
         return gimp
 
     def transf_sp_basis(self, g):
-        setup = Setup(g.beta, 0, 0, 0, 0, 1)
+        setup = Setup(g.mesh.beta, 0, 0, 0, 0, 1)
         result = setup.reblock_ksum.transform(g)
         return result
-    
+
     def calc_correlation_functions(self, se_cdmft, hopping, nk, gk_on_the_fly, hk_on_the_fly):
         self.report('calculating lattice correlation functions...')
         se_cdmft.copy()
@@ -79,20 +85,21 @@ class JosephsonExchangeCommon:
         blocknames = [k for k in se_cdmft.indices]
         blockindices = [se_cdmft[bn].indices for bn in blocknames]
         weights_r = [1] * hopping.n_r
-        glat = LatticeGreensfunction(blocknames, blockindices, hopping.r, hopping.h_r, nk, se_cdmft, mu, weights_r, gk_on_the_fly = gk_on_the_fly, hk_on_the_fly = hk_on_the_fly)
+        glat = LatticeGreensfunction(blocknames, blockindices, hopping.r, hopping.h_r, nk,
+                                     se_cdmft, mu, weights_r, gk_on_the_fly=gk_on_the_fly, hk_on_the_fly=hk_on_the_fly)
         return se_cdmft, glat
 
     def calc_values(self, se, glat, rjs):
         self.report('calculating J...')
         ri = tuple([0]*glat.d)
         niw = int(.5*len(se.mesh))
-        gtmp = BlockGf(name_block_generator = [[j, GfImFreq(beta = se.beta, n_points = niw,
-                                                            indices = [0])] for j in se.indices])
-        gsgs = BlockGf(name_block_generator = [[j, GfImFreq(beta = se.beta, n_points = niw,
-                                                            indices = [0])] for j in se.indices])
-        fsfs = BlockGf(name_block_generator = [[j, GfImFreq(beta = se.beta, n_points = niw,
-                                                            indices = [0])] for j in se.indices])
-        j_r = np.zeros([len(rjs),3])
+        gtmp = BlockGf(name_block_generator=[[j, GfImFreq(beta=se.mesh.beta, n_points=niw,
+                                                          indices=[0])] for j in se.indices], make_copies=False)
+        gsgs = BlockGf(name_block_generator=[[j, GfImFreq(beta=se.mesh.beta, n_points=niw,
+                                                          indices=[0])] for j in se.indices], make_copies=False)
+        fsfs = BlockGf(name_block_generator=[[j, GfImFreq(beta=se.mesh.beta, n_points=niw,
+                                                          indices=[0])] for j in se.indices], make_copies=False)
+        j_r = np.zeros([len(rjs), 3])
         bn = '0'
         for i_rj, rj in enumerate(rjs):
             self.report_detail('...'+str(rj)+'...')
@@ -102,32 +109,40 @@ class JosephsonExchangeCommon:
             gsgs00 = gsgs[bn][0, 0]
             fsfs00 = fsfs[bn][0, 0]
             self.report_detail('calculating g_lat_ij...')
-            glatij = glat[ri,rj][bn]
+            glatij = glat[ri, rj][bn]
             glatji = glatij
             self.report_detail('calculating scalar products...')
-            for i, j, k, l in itt.product(*[range(4)]*4): # could be parallelized
-                if j!=k and not((j,k) in [(1,5),(5,1),(2,6),(6,2)]): continue
-                if l!=i and not((l,i) in [(1,5),(5,1),(2,6),(6,2)]): continue
-                gtmp00 += glatij[0+i,4+j]*se[bn][0+j,4+k]*glatji[0+k,4+l]*se[bn][0+l,4+i]
-                gtmp00 -= glatij[0+i,0+j]*se[bn][0+j,4+k]*glatji[4+k,4+l]*se[bn][0+l,4+i]
-                fsfs00 += glatij[0+i,4+j]*se[bn][0+j,4+k]*glatji[0+k,4+l]*se[bn][0+l,4+i]
-                gsgs00 -= glatij[0+i,0+j]*se[bn][0+j,4+k]*glatji[4+k,4+l]*se[bn][0+l,4+i]
+            # could be parallelized
+            for i, j, k, l in itt.product(*[range(4)]*4):
+                if j != k and not((j, k) in [(1, 5), (5, 1), (2, 6), (6, 2)]):
+                    continue
+                if l != i and not((l, i) in [(1, 5), (5, 1), (2, 6), (6, 2)]):
+                    continue
+                gtmp00 += glatij[0+i, 4+j]*se[bn][0+j, 4+k] * \
+                    glatji[0+k, 4+l]*se[bn][0+l, 4+i]
+                gtmp00 -= glatij[0+i, 0+j]*se[bn][0+j, 4+k] * \
+                    glatji[4+k, 4+l]*se[bn][0+l, 4+i]
+                fsfs00 += glatij[0+i, 4+j]*se[bn][0+j, 4+k] * \
+                    glatji[0+k, 4+l]*se[bn][0+l, 4+i]
+                gsgs00 -= glatij[0+i, 0+j]*se[bn][0+j, 4+k] * \
+                    glatji[4+k, 4+l]*se[bn][0+l, 4+i]
             if np.sum(np.array(ri)-np.array(rj)) == 0:
-                for i, j in itt.product(*[range(4)]*2): # could be parallelized
-                    if j!=i and not((j,k) in [(1,5),(5,1),(2,6),(6,2)]): continue
-                    gtmp00 += glatij[0+i,4+j]*se[bn][0+j,4+i]
+                for i, j in itt.product(*[range(4)]*2):  # could be parallelized
+                    if j != i and not((j, k) in [(1, 5), (5, 1), (2, 6), (6, 2)]):
+                        continue
+                    gtmp00 += glatij[0+i, 4+j]*se[bn][0+j, 4+i]
                 gtmp00 << -1*gtmp00
             self.values[rj] = gtmp.total_density().real
             self.fsfs[rj] = fsfs.total_density().real
             self.gsgs[rj] = gsgs.total_density().real
 
-    def save_to_h5(self, h5name = None, groupname = 'josephsonexchange', dict_to_save = None):
+    def save_to_h5(self, h5name=None, groupname='josephsonexchange', dict_to_save=None):
         if h5name is None:
             self._save(self.h5name_cdmft, groupname, dict_to_save)
         else:
             self._save(h5name, groupname, dict_to_save)
 
-    def _save(self, h5name, groupname, dict_to_save = None):
+    def _save(self, h5name, groupname, dict_to_save=None):
         if dict_to_save is None:
             dict_to_save = self.values
         if mpi.is_master_node():
@@ -155,12 +170,13 @@ class JosephsonExchangeCommon:
 class JosephsonExchange2D(JosephsonExchangeCommon):
 
     def __init__(self, h5name_cdmft, nk, niw, tnn, tnnn, tz,
-                 rjs = [(0,0), (1,0), (1,1), (2,0), (2,1), (2,2), (3,0)],
-                 verbose = True, loops = [-1], gk_on_the_fly = True, hk_on_the_fly = True):
+                 rjs=[(0, 0), (1, 0), (1, 1), (2, 0), (2, 1), (2, 2), (3, 0)],
+                 verbose=True, loops=[-1], gk_on_the_fly=True, hk_on_the_fly=True):
 
         tz = 0
         self.h5name_cdmft = h5name_cdmft
-        self.parameters = {'nk':nk, 'niw': niw, 'tnn': tnn, 'tnnn': tnnn, 'tz': tz}
+        self.parameters = {'nk': nk, 'niw': niw,
+                           'tnn': tnn, 'tnnn': tnnn, 'tz': tz}
         self.verbose = verbose
         self.values = dict()
         self.fsfs = dict()
@@ -169,7 +185,8 @@ class JosephsonExchange2D(JosephsonExchangeCommon):
         se_cdmft, mu = self.load_cdmft(h5name_cdmft, niw, loops)
         se_cdmft = self.transf_sp_basis(se_cdmft)
         hop = self.get_hopping(mu, tnn, tnnn)
-        se, glat = self.calc_correlation_functions(se_cdmft, hop, nk, gk_on_the_fly, hk_on_the_fly)
+        se, glat = self.calc_correlation_functions(
+            se_cdmft, hop, nk, gk_on_the_fly, hk_on_the_fly)
         self.calc_values(se, glat, rjs)
 
     def get_hopping(self, mu, tnn, tnnn, *args):
